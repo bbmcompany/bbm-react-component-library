af01323fdd6814cb3727331407d3c07d8144f2e2
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-08-04 11:08:46 +0300

    rcl (dimensions) dimensions (height & width) scss added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
-import '../src/stories/UI/styles/dimensions/b-margin.scss';
-import '../src/stories/UI/styles/dimensions/b-padding.scss';
+import '../src/stories/UI/styles/dimensions/b-spacing.scss';
+import '../src/stories/UI/styles/dimensions/b-sizing.scss';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
