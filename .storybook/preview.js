8ae6af3c1227bfb57567ce78ea4f9bb65db5d38b
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-08-04 23:20:20 +0300

    rcl (dimensions - 102) Start adding dimension system (border) to utilities. border, border-top, border-bottom properties have been added.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import '../src/stories/UI/styles/dimensions/b-spacing.scss';
 import '../src/stories/UI/styles/dimensions/b-sizing.scss';
-
+import '../src/stories/UI/styles/border/border.scss';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
---
+++ src/stories/UI/utilities/Border/Border.stories.tsx
@@ -0,0 +1,63 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { C3Border } from './Border'
+import './border-stories.scss'
+
+export default {
+    title : 'UTILITIES/C3Border',
+    component: C3Border,
+    argTypes: {}
+} as ComponentMeta<typeof C3Border>
+
+const BorderTemplate: ComponentStory<typeof C3Border> = args => (
+    <div className={'border'}>
+        <C3Border {...args} className={'b-border-0 b-ml-10'}>Border 0</C3Border>
+        <C3Border {...args} className={'b-border-1'}>Border 1</C3Border>
+        <C3Border {...args} className={'b-border-2'}>Border 2</C3Border>
+        <C3Border {...args} className={'b-border-3'}>Border 3</C3Border>
+        <C3Border {...args} className={'b-border-4 b-mr-10'}>Border 4</C3Border>
+        <C3Border {...args} className={'b-border-5 b-ml-10'}>Border 5</C3Border>
+        <C3Border {...args} className={'b-border-10 '}>Border 10</C3Border>
+        <C3Border {...args} className={'b-border-20'}>Border 20</C3Border>
+        <C3Border {...args} className={'b-border-25'}>Border 25</C3Border>
+        <C3Border {...args} className={'b-border-50 b-mr-10'}>Border 50</C3Border>
+        <C3Border {...args} className={'b-border-75 b-ml-10'}>Border 75</C3Border>
+        <C3Border {...args} className={'b-border-100 b-mr-10'}>Border 100</C3Border>
+    </div>
+);
+export const Border = BorderTemplate.bind({});
+
+const BorderTopTemplate: ComponentStory<typeof C3Border> = args => (
+    <div className={'border-top'}>
+        <C3Border {...args} className={'b-border-t-0 b-ml-10'}>Border Top 0</C3Border>
+        <C3Border {...args} className={'b-border-t-1'}>Border Top 1</C3Border>
+        <C3Border {...args} className={'b-border-t-2'}>Border Top 2</C3Border>
+        <C3Border {...args} className={'b-border-t-3'}>Border Top 3</C3Border>
+        <C3Border {...args} className={'b-border-t-4 b-mr-10'}>Border Top 4</C3Border>
+        <C3Border {...args} className={'b-border-t-5 b-ml-10'}>Border Top 5</C3Border>
+        <C3Border {...args} className={'b-border-t-10 '}>Border Top 10</C3Border>
+        <C3Border {...args} className={'b-border-t-20'}>Border Top 20</C3Border>
+        <C3Border {...args} className={'b-border-t-25'}>Border Top 25</C3Border>
+        <C3Border {...args} className={'b-border-t-50 b-mr-10'}>Border Top 50</C3Border>
+        <C3Border {...args} className={'b-border-t-75 b-ml-10'}>Border Top 75</C3Border>
+        <C3Border {...args} className={'b-border-t-100 b-mr-10'}>Border Top 100</C3Border>
+    </div>
+);
+export const BorderTop = BorderTopTemplate.bind({});
+
+const BorderBottomTemplate: ComponentStory<typeof C3Border> = args => (
+    <div className={'border-bottom'}>
+        <C3Border {...args} className={'b-border-b-0 b-ml-10'}>Border Bottom 0</C3Border>
+        <C3Border {...args} className={'b-border-b-1'}>Border Bottom 1</C3Border>
+        <C3Border {...args} className={'b-border-b-2'}>Border Bottom 2</C3Border>
+        <C3Border {...args} className={'b-border-b-3'}>Border Bottom 3</C3Border>
+        <C3Border {...args} className={'b-border-b-4 b-mr-10'}>Border Bottom 4</C3Border>
+        <C3Border {...args} className={'b-border-b-5 b-ml-10'}>Border Bottom 5</C3Border>
+        <C3Border {...args} className={'b-border-b-10 b-ml-10'}>Border Bottom 10</C3Border>
+        <C3Border {...args} className={'b-border-b-20'}>Border Bottom 20</C3Border>
+        <C3Border {...args} className={'b-border-b-25'}>Border Bottom 25</C3Border>
+        <C3Border {...args} className={'b-border-b-50 b-mr-10'}>Border Bottom 50</C3Border>
+        <C3Border {...args} className={'b-border-b-75 b-ml-10'}>Border Bottom 75</C3Border>
+        <C3Border {...args} className={'b-border-b-100 b-mr-10'}>Border Bottom 100</C3Border>
+    </div>
+);
+export const BorderBottom = BorderBottomTemplate.bind({});
