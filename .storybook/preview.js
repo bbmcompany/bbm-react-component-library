816a50e61a6cce9fd363527b4e66f78bb3f81d73
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-08-04 10:24:35 +0300

    rcl (dimensions) dimensions (padding) scss added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '../src/stories/UI/styles/dimensions/b-margin.scss';
+import '../src/stories/UI/styles/dimensions/b-padding.scss';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
