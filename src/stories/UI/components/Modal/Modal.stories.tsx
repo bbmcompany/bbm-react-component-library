ecd4d8906289d1c97dc2d53684d7ba156d889d28
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-08-05 15:34:17 +0300

    rcl (modal -102)  modal component function added.

--- src/stories/UI/components/Modal/Modal.stories.tsx
+++ src/stories/UI/components/Modal/Modal.stories.tsx
@@ -1,18 +1,28 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { C3Modal } from "./Modal";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {C3Modal} from "./Modal";
 
 export default {
     title: 'COMPONENTS/C3Modal',
     component: C3Modal,
-    argTypes: {
-
-    }
+    argTypes: {}
 } as ComponentMeta<typeof C3Modal>
 
-const VariantsTemplate : ComponentStory<typeof C3Modal> = args => (
-    <div>
-        <div className={'modal-variants'}>
-            <C3Modal {...args}>
+const func = () => {
+    document.getElementById('wrapper')!.style.display = 'none';
+}
+
+const VariantsTemplate: ComponentStory<typeof C3Modal> = args => (
+    <div className={'modal'}>
+        <div className={'modal-variants b-m-10'}>
+            <C3Modal {...args} modalButtonFunction={() => func()}>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Donec ipsum mauris, dapibus non sapien in, tristique tristique lorem.</p>
+                <p>In hac habitasse platea dictumst.</p>
+            </C3Modal>
+        </div>
+
+        <div className={'modal-variants b-m-10'}>
+            <C3Modal {...args} modalButtonFunction={() => func()} modalButtonText={'Close'}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 <p>Donec ipsum mauris, dapibus non sapien in, tristique tristique lorem.</p>
                 <p>In hac habitasse platea dictumst.</p>
--- src/stories/UI/components/Modal/Modal.tsx
+++ src/stories/UI/components/Modal/Modal.tsx
@@ -1,21 +1,25 @@
-import { C3Button } from "../Button/Button";
+import {C3Button} from "../Button/Button";
 import './modal.scss'
+import './modal-stoires.scss'
 interface C3ModalProps {
     className?: string;
     title?: string;
     modalButtonText?: string;
+    modalButtonFunction?: any;
     buttonText?: string;
     children?: any;
 }
 
 export const C3Modal = ({
                             children = undefined,
+                            modalButtonFunction = () => {
+                            },
                             title = 'Custom Title Here',
-                            modalButtonText = 'Close',
+                            modalButtonText = 'Function Button',
                             buttonText = 'Open Modal',
                             className = ' ',
                             ...props
-                         }: C3ModalProps) => {
+                        }: C3ModalProps) => {
     const openModal = () => {
         document.getElementById('wrapper')!.style.display = 'flex';
     }
@@ -28,11 +32,20 @@ export const C3Modal = ({
                       onClick={() => openModal()}>{buttonText}</C3Button>
             <div className={'bbm-rcl-modal--wrapper'} id={'wrapper'}>
                 <div className={'body b-m-auto b-p-25 b-border-5'}>
-                    <div className={'title'}>{title}
+                    <div className={'title-group'}>
+                        <div className={'title'}>{title}</div>
+                        <C3Button
+                            className='b-m-5'
+                            icon='close'
+                            iconButton
+                            onClick={() => closeModal()}
+                            primary
+                            transparent
+                        />
                     </div>
                     <div className={'content b-my-25'}>{children}</div>
                     <C3Button className={'close-button b-m-5'} danger size={'large'}
-                              onClick={() => closeModal()}>{modalButtonText}</C3Button>
+                              onClick={() => modalButtonFunction()}>{modalButtonText}</C3Button>
                 </div>
             </div>
         </div>
