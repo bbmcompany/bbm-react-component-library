cee9f902a2cfe677dde20ae0d798b1a04d300181
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-08-10 13:45:45 +0300

    rcl (code-review 107 : label) code review and design changes made

--- src/stories/UI/components/Label/Label.stories.tsx
+++ src/stories/UI/components/Label/Label.stories.tsx
@@ -1,20 +1,17 @@
 import {C3Label} from "./Label";
 import {ComponentMeta, ComponentStory} from "@storybook/react";
 import './label-stories.scss';
+
 export default {
     title: 'COMPONENTS/C3Label',
     component: C3Label,
     argTypes: {
-        options: {
+        iconPosition: {
             control: {
                 type: 'select',
                 options: {
-                    'In Progress': 'in-progress',
-                    'Approved': 'approved',
-                    'Warning': 'warning',
-                    'Denied': 'denied',
-                    'Offer': 'offer',
-                    'Winner': 'winner',
+                    'Left': 'left',
+                    'Right': 'right',
                 }
             }
         },
@@ -22,12 +19,10 @@ export default {
             control: {
                 type: 'select',
                 options: {
-                    'Small': 'small',
-                    'Medium': 'medium',
-                    'Large': 'large',
-                    'X Large': 'x-large',
-                    '2X Large': '2x-large',
-                    '3X Large': '3x-large',
+                    'xs': 'xs',
+                    's': 's',
+                    'm': 'm',
+                    'l': 'l',
                 }
             }
         },
@@ -46,46 +41,46 @@ export default {
 
 const LabelVariantsTemplate: ComponentStory<typeof C3Label> = args => (
     <div className={'label'}>
-        <C3Label{...args} options={'in-progress'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'approved'} className={'b-m-5'}>Approved</C3Label>
-        <C3Label{...args} options={'warning'} className={'b-m-5'}>Warning</C3Label>
-        <C3Label{...args} options={'denied'} className={'b-m-5'}>Denied</C3Label>
-        <C3Label{...args} options={'offer'} className={'b-m-5'}>Offer</C3Label>
-        <C3Label{...args} options={'winner'} className={'b-m-5'}>Winner</C3Label>
-        <C3Label{...args} className={'b-m-5'}>Default</C3Label>
+        <C3Label{...args} primary className={'b-m-5 b-border-32'}>Primary</C3Label>
+        <C3Label{...args} secondary className={'b-m-5 b-border-1'}>Secondary</C3Label>
+        <C3Label{...args} tertiary className={'b-m-5 b-border-2'}>Tertiary</C3Label>
+        <C3Label{...args} orange className={'b-m-5 b-border-3'}>Orange</C3Label>
+        <C3Label{...args} grey className={'b-m-5 b-border-4'}>Grey</C3Label>
+        <C3Label{...args} error className={'b-m-5'}>Error</C3Label>
+        <C3Label{...args} success className={'b-m-5'}>Success</C3Label>
+        <C3Label{...args} warning className={'b-m-5'}>Warning</C3Label>
+        <C3Label{...args} info className={'b-m-5'}>Info</C3Label>
     </div>
 );
 export const Variants = LabelVariantsTemplate.bind({});
 
 const LabelWithSizesTemplate: ComponentStory<typeof C3Label> = args => (
     <div className={'label start'}>
-        <C3Label{...args} options={'in-progress'} size={'small'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'in-progress'} size={'medium'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'in-progress'} size={'large'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'in-progress'} size={'x-large'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'in-progress'} size={'2x-large'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'in-progress'} size={'3x-large'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'in-progress'} className={'b-m-5'}>Default</C3Label>
+        <C3Label{...args} size={'xs'} className={'b-m-5'}>In Progress</C3Label>
+        <C3Label{...args} size={'s'} className={'b-m-5'}>In Progress</C3Label>
+        <C3Label{...args} size={'m'} className={'b-m-5'}>In Progress</C3Label>
+        <C3Label{...args} size={'l'} className={'b-m-5'}>In Progress</C3Label>
     </div>
 );
 export const LabelWithSizes = LabelWithSizesTemplate.bind({});
 
 const LabelWithIconTemplate: ComponentStory<typeof C3Label> = args => (
     <div className={'label'}>
-        <C3Label{...args} options={'in-progress'} icon={'academy'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'approved'} icon={'archive'} className={'b-m-5'}>Approved</C3Label>
-        <C3Label{...args} options={'warning'} icon={'alert'} className={'b-m-5'}>Warning</C3Label>
-        <C3Label{...args} options={'winner'} icon={'award'} className={'b-m-5'}>Winner</C3Label>
+        <C3Label{...args} icon={'academy'} className={'b-m-5'}>Academy</C3Label>
+        <C3Label{...args} icon={'archive'} className={'b-m-5'}>Approved</C3Label>
+        <C3Label{...args} icon={'alert'} className={'b-m-5'}>Warning</C3Label>
+        <C3Label{...args}  className={'b-m-5 b-border-32'}>Warning</C3Label>
+        <C3Label{...args} icon={'award'} className={'b-m-5 b-border-32'}>Winner</C3Label>
     </div>
 );
 export const LabelWithIcon = LabelWithIconTemplate.bind({});
 
 const LabelIconTemplate: ComponentStory<typeof C3Label> = args => (
     <div className={'label'}>
-        <C3Label{...args} options={'in-progress'} iconLabel icon={'academy'} className={'b-m-5'}>In Progress</C3Label>
-        <C3Label{...args} options={'approved'} iconLabel icon={'archive'} className={'b-m-5'}>Approved</C3Label>
-        <C3Label{...args} options={'warning'} iconLabel icon={'alert'} className={'b-m-5'}>Warning</C3Label>
-        <C3Label{...args} options={'winner'} iconLabel icon={'award'} className={'b-m-5'}>Winner</C3Label>
+        <C3Label{...args} iconLabel icon={'academy'} className={'b-m-5'}/>
+        <C3Label{...args} iconLabel icon={'archive'} className={'b-m-5'}/>
+        <C3Label{...args} iconLabel icon={'alert'} className={'b-m-5'}/>
+        <C3Label{...args} iconLabel icon={'award'} className={'b-m-5 b-border-100'}/>
     </div>
 );
 export const LabelIcon = LabelIconTemplate.bind({});
--- src/stories/UI/components/Label/Label.tsx
+++ src/stories/UI/components/Label/Label.tsx
@@ -5,40 +5,58 @@ interface C3LabelProps {
     size?: string;
     children?: string;
     color?: string;
-    options?: string;
     backgroundColor?: string;
     icon?: string;
     iconLabel?: boolean;
+    iconPosition?: string;
+    primary?: boolean;
+    secondary?: boolean;
+    tertiary?: boolean;
+    error?: boolean;
+    warning?: boolean;
+    success?: boolean;
+    info?: boolean;
+    orange?: boolean;
+    grey?: boolean;
+    solid?: boolean;
+    outline?: boolean;
     className?: string;
 }
 
 export const C3Label = ({
-                            size = 'medium',
+                            size = 's',
                             children = ' ',
                             color = ' ',
-                            options = ' ',
                             backgroundColor = ' ',
                             icon = ' ',
                             className = ' ',
                             iconLabel = false,
+                            iconPosition = 'Left',
                             ...props
                         }: C3LabelProps) => {
+    let variantClasses = ' ';
     if (iconLabel && icon == ' ') {
         icon = 'academy';
     }
 
+    for (const [key, value] of Object.entries(props)) {
+        if (value) {
+            variantClasses += ` bbm-rcl-label--${key}`;
+        }
+    }
+
     return (
-        <div className={['bbm-rcl-label', 'b-border-5', className].join(' ')}>
+        <div className={['bbm-rcl-label'].join(' ')}>
              <span
-                 className={[`bbm-rcl-label--${options}`, `bbm-rcl-label--${size}`,'b-border-1'].join(' ')}
+                 className={['bbm-rcl-label', variantClasses, `bbm-rcl-label--${size}`, className].join(' ')}
                  style={{color: color, backgroundColor: backgroundColor}}
              >
     {icon == ' ' ? <>
         <div className={"text"}>
             {children}
         </div>
-    </> : <div className={'icon-container'}>
-        <C3Icon icon={icon}></C3Icon>
+    </> : <div className={['icon-container', `--${iconPosition}`].join(' ')}>
+        <C3Icon icon={icon} size={size}></C3Icon>
         <div className={iconLabel ? 'secret-text' : 'text'}>
             {children}
         </div>
